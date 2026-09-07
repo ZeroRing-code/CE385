44c5fddbe39a765f724deab3387c8c9d891e4ce4
ZeroRing <partyrinry@gmail.com>
2026-09-07 12:33:31 +0700

    ex1

---
+++ Worlshop3/ex1-callback.js
@@ -0,0 +1,23 @@
+
+STUDENTS = [
+    { id: "123", name: "สมชาย", major: "CE", score: 85 },
+    { id: "456", name: "สมหญิง", major: "CE", score: 90 },
+    { id: "789", name: "สมปอง", major: "IE", score: 75 },
+];
+function fetchStudentSafe(id, callback) {setTimeout(() => {
+    if (typeof id !== "string" || id.trim() === "") {
+        callback(new Error(" รหัส นักศึกษาไม่ถูกต้อง"));
+    }
+    const student = STUDENTS.find((s) => s.id === id);
+    if (!student) {
+        callback(new Error(`ไม่พบรหัสนักศึกษา ${id}`));
+    }
+    callback(null, student);
+    if (student) {
+        console.log(`พบนักศึกษา: ${student.name} เลขทะเบียน: ${student.id} สาขา: ${student.major} คะแนน: ${student.score}`);
+        
+    }
+}, 400);
+
+
+console.log("กำลังค้นหานักศึกษา...");
